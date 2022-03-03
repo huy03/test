bc83396b59380957fc781e477c67e5bf556f5598
huy03 <huydao1008@gmail.com>
2022-03-03 10:28:39 +0700

    add file

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    char str[300];
+    cin.getline(str,256);
+    char *tmp;
+    tmp = strtok (str, " ");
+    while (tmp!=NULL){
+        for (int i=strlen(tmp)-1; i>=0; i--){
+            cout << tmp[i];
+        }
+        cout << " ";
+        tmp = strtok(NULL," ");
+    }
+    return 0;
+}
